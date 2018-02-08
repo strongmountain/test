0474804c8810749e5c348b95b5e734d28222e7e5
Kyle Hornberg <khornberg@users.noreply.github.com>
2018-02-08 12:03:43 -0600

    Second commit

--- test.py
+++ test.py
@@ -2,3 +2,4 @@ from jose import jwt
 token = jwt.encode({'key': 'value'}, 'secret', algorithm='HS256')
 assert token == u'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJ2YWx1ZSJ9.FG-8UppwHaFp1LgRYQQeS6EDQF7_6-bMFegNucHjmWg'
 assert True
+assert True == True
