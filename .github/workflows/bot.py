701bd6258e89a3311ed9d8a7cca50e22a8a29c73
Kyle Hornberg <kyle.hornberg@gmail.com>
2020-01-17 13:50:32 -0600

    fix script

--- .github/workflows/bot.py
+++ .github/workflows/bot.py
@@ -3,5 +3,5 @@ import os
 from octokit import Octokit
 
 
-octokit = Octokit(auth='token', token=os.get('GITHUB_TOKEN'))
+octokit = Octokit(auth='token', token=os.environ.get('GITHUB_TOKEN'))
 print(octokit.repos.get_for_org(org='strongmountain').json())
