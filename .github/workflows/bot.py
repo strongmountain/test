b307b1c3ab9e94dc5029b5de6bd8208cbeabda3c
Kyle Hornberg <kyle.hornberg@gmail.com>
2020-01-17 15:31:20 -0600

    List repos

--- .github/workflows/bot.py
+++ .github/workflows/bot.py
@@ -4,4 +4,4 @@ from octokit import Octokit
 
 
 octokit = Octokit(auth='token', token=os.environ.get('GITHUB_TOKEN'))
-print(octokit.repos.get_for_org(org='strongmountain').json())
+print(octokit.repos.list().json)
