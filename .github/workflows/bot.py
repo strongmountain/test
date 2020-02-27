48ca42097a6c493b8e281be596da29d57b18d327
Kyle Hornberg <kyle.hornberg@gmail.com>
2020-02-27 14:05:58 -0600

    add header

--- .github/workflows/bot.py
+++ .github/workflows/bot.py
@@ -8,10 +8,10 @@ octokit = Octokit(auth="token", token=os.environ.get("GITHUB_TOKEN"))
 owner, repo = os.environ.get("GITHUB_REPOSITORY").split("/")
 ref = os.environ.get("GITHUB_REF")
 
-number = ref.split("/")[2] if "pull" in ref else None
-octokit.issues.create_comment(
-    owner=owner, repo=repo, issue_number=number, body=f"**Comment from {os.environ.get('GITHUB_RUN_ID')}**"
-)
+# number = ref.split("/")[2] if "pull" in ref else None
+# octokit.issues.create_comment(
+#     owner=owner, repo=repo, issue_number=number, body=f"**Comment from {os.environ.get('GITHUB_RUN_ID')}**"
+# )
 
 event_path = os.environ.get("GITHUB_EVENT_PATH")
 with open(event_path) as event_file:
@@ -22,5 +22,10 @@ params = {
     "head_sha": event["pull_request"]["head"]["sha"],
     "output": {"title": "Mighty Readme report", "summary": "", "text": ""},
 }
-response = octokit.checks.create_a_check_run(owner=owner, repo=repo, **params)
+headers = {
+    "accept": "application/vnd.github.antiope-preview+json",
+    "Content-Type": "application/json",
+}
+
+response = octokit.checks.create_a_check_run(owner=owner, repo=repo, **params, **headers)
 print(response.json)
