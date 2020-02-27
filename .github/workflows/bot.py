2ab5c5ef40f93e2d8e728d5be9ed2dbc5a12ae20
Kyle Hornberg <kyle.hornberg@gmail.com>
2020-02-27 13:30:36 -0600

    create a check run

--- .github/workflows/bot.py
+++ .github/workflows/bot.py
@@ -9,3 +9,13 @@ ref = os.environ.get('GITHUB_REF')
 
 number = ref.split('/')[2] if 'pull' in ref else None
 octokit.issues.create_comment(owner=owner, repo=repo, issue_number=number, body=f"**Comment from {os.environ.get('GITHUB_RUN_ID')}**")
+
+event_path = os.environ.get('GITHUB_EVENT_PATH')
+with open(event_path) as event_file:
+    event = json.load(event_file))
+
+params = {
+    "name": "check run!",
+    "head_sha": event["pull_request"]["head"]["sha"]
+}
+octokit.checks.runs.create_a_check_run(owner=owner, repo=repo, **params)
