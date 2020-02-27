ecf28aa8b017c552efd0fc91dbc9828301b9f1a4
Kyle Hornberg <kyle.hornberg@gmail.com>
2020-02-27 13:43:59 -0600

    this is why we have flake8
    which of course was not installed

--- .github/workflows/bot.py
+++ .github/workflows/bot.py
@@ -1,21 +1,21 @@
 import os
+import json
 
 from octokit import Octokit
 
 
-octokit = Octokit(auth='token', token=os.environ.get('GITHUB_TOKEN'))
-owner, repo = os.environ.get('GITHUB_REPOSITORY').split('/')
-ref = os.environ.get('GITHUB_REF')
+octokit = Octokit(auth="token", token=os.environ.get("GITHUB_TOKEN"))
+owner, repo = os.environ.get("GITHUB_REPOSITORY").split("/")
+ref = os.environ.get("GITHUB_REF")
 
-number = ref.split('/')[2] if 'pull' in ref else None
-octokit.issues.create_comment(owner=owner, repo=repo, issue_number=number, body=f"**Comment from {os.environ.get('GITHUB_RUN_ID')}**")
+number = ref.split("/")[2] if "pull" in ref else None
+octokit.issues.create_comment(
+    owner=owner, repo=repo, issue_number=number, body=f"**Comment from {os.environ.get('GITHUB_RUN_ID')}**"
+)
 
-event_path = os.environ.get('GITHUB_EVENT_PATH')
+event_path = os.environ.get("GITHUB_EVENT_PATH")
 with open(event_path) as event_file:
     event = json.load(event_file)
 
-params = {
-    "name": "check run!",
-    "head_sha": event["pull_request"]["head"]["sha"]
-}
+params = {"name": "check run!", "head_sha": event["pull_request"]["head"]["sha"]}
 octokit.checks.runs.create_a_check_run(owner=owner, repo=repo, **params)
